f6349503ab3dc6a80583b196c95827ee067323bd
Craig Jellick <craig@acorn.io>
2024-12-06 19:18:43 -0700

    hackery 1
    Signed-off-by: Craig Jellick <craig@acorn.io>

--- pkg/router/handler.go
+++ pkg/router/handler.go
@@ -322,17 +322,6 @@ func (m *HandlerSet) handle(gvk schema.GroupVersionKind, key string, unmodifiedO
 	}
 
 	if handles {
-		m.watchingLock.Lock()
-		keys := maps.Keys(m.watching)
-		m.watchingLock.Unlock()
-		newObj, err := m.save.save(unmodifiedObject, req, resp, keys)
-		if err != nil {
-			if err := m.handleError(req, resp, err); err != nil {
-				return nil, err
-			}
-		}
-		req.Object = newObj
-
 		if resp.delay > 0 {
 			if err := m.backend.Trigger(gvk, key, resp.delay); err != nil {
 				return nil, err
